fcf3f2e5777ee59063ecf0dd9b2b24f78558a764
Matt Whiting <whitingm@usc.edu>
2024-05-05 10:04:23 -0700

    Working on mega laser

--- Assets/Scripts/Weapons/LaserBeam.cs
+++ Assets/Scripts/Weapons/LaserBeam.cs
@@ -7,12 +7,13 @@ public class LaserBeam : MonoBehaviour
     public Texture[] m_textures;
     public float m_animSpeed = 12.0f;   // frames per second
     public float m_scrollSpeed = -1.0f;
-    public GameObject m_hitSplash;
     LineRenderer m_line;
     Material m_mat;
     float m_animTimer;
     int m_curFrame;
-    float m_offset = 0.0f;
+    float m_uvOffset = 0.0f;
+    protected SpriteRenderer m_ownerSprite;
+    protected Vector3 m_localPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -28,43 +29,57 @@ public class LaserBeam : MonoBehaviour
         m_line.material = m_mat;
         m_animTimer = 0.0f;
         m_curFrame = -1;
-        m_hitSplash.SetActive(false);
+
+        m_ownerSprite = transform.parent.GetComponent<SpriteRenderer>();
+        m_localPosition = transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // flip with owner
+        Vector3 pos = m_localPosition;
+        if (m_ownerSprite.flipX)
+        {
+            pos.x = -pos.x;
+        }
+        
+        transform.localPosition = pos;
+
         // Collision
         Vector3 rightSide = new Vector3(1.0f, 0.5f, 0.0f);
+        if (m_ownerSprite.flipX)
+        {
+            rightSide.x = -1.0f;
+        }
         rightSide = Camera.main.ViewportToWorldPoint(rightSide);
         Vector3 start = transform.position;
+        m_line.SetPosition(0, start);
         float dist = rightSide.x - start.x;
         string[] masks = {
             "Enemy",
             "EnemyRocket",
-            "Egg"
+            "Egg",
+            "Worm"
         };
         LayerMask layerMask = LayerMask.GetMask(masks);
         RaycastHit2D hit = Physics2D.Raycast(start, Vector3.right, dist, layerMask);
         if (hit)
         {
             m_line.SetPosition(1, hit.point);
-            m_hitSplash.SetActive(true);
-            m_hitSplash.transform.position = hit.point;
         }
         else
         {
             Vector3 end = start;
             end.x = rightSide.x;
             m_line.SetPosition(1, end);
-            m_hitSplash.SetActive(false);
         }
 
         // Animation
         float dt = Time.deltaTime;
         m_animTimer += m_animSpeed * dt;
-        m_offset += m_scrollSpeed * dt;
-        m_offset = m_offset - (int)m_offset;
+        m_uvOffset += m_scrollSpeed * dt;
+        m_uvOffset = m_uvOffset - (int)m_uvOffset;
         while (m_animTimer >= m_textures.Length)
         {
             m_animTimer -= m_textures.Length;
@@ -75,6 +90,6 @@ public class LaserBeam : MonoBehaviour
             m_curFrame = animFrame;
             m_mat.SetTexture("_MainTex", m_textures[m_curFrame]);
         }
-        m_mat.SetTextureOffset("_MainTex", new Vector2(m_offset, 0.0f));
+        m_mat.SetTextureOffset("_MainTex", new Vector2(m_uvOffset, 0.0f));
     }
 }
